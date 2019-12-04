da776b3096986f88f62cb7d259123d08aa76777c
Samuel Simonetti <samuelribeirosimonetti10@gmail.com>
2019-12-03 22:16:47 -0300

    Add files via upload

---
+++ build/classes/ch/makery/address/db/popular_db.sql
@@ -0,0 +1,6 @@
+INSERT INTO Pessoa VALUES
+('14781092012', 'Samuel', 'Simonetti', 'Rua Paim', 'Sao Paulo', '12341341', '07-05-2002'),
+('14781092013', 'Mateus', 'Simonetti', 'Rua Paim', 'Sao Paulo', '12341341', '02-01-1999'),
+('14781092014', 'Paulo', 'Simonetti', 'Rua Paim', 'Sao Paulo', '12341341', '27-04-1996'),
+('14781092015', 'Luiz', 'Simonetti', 'Rua Paim', 'Sao Paulo', '12341341', '04-03-1965'),
+('14781092016', 'João', 'Simonetti', 'Rua Paim', 'Sao Paulo', '12341341', '14-10-2014');
